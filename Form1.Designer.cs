2600370e6df6000d5c8f644562f8e8a39e4d8230
Vijay Karla <vijays80@gmail.com>
2017-08-08 08:32:24 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ExcelCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRNfile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.bntCAFile = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRNfile
+            // 
+            this.btnRNfile.Location = new System.Drawing.Point(37, 68);
+            this.btnRNfile.Name = "btnRNfile";
+            this.btnRNfile.Size = new System.Drawing.Size(75, 23);
+            this.btnRNfile.TabIndex = 0;
+            this.btnRNfile.Text = "RN File";
+            this.btnRNfile.UseVisualStyleBackColor = true;
+            this.btnRNfile.Click += new System.EventHandler(this.btnRN_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // bntCAFile
+            // 
+            this.bntCAFile.Location = new System.Drawing.Point(166, 68);
+            this.bntCAFile.Name = "bntCAFile";
+            this.bntCAFile.Size = new System.Drawing.Size(75, 23);
+            this.bntCAFile.TabIndex = 1;
+            this.bntCAFile.Text = "CA File";
+            this.bntCAFile.UseVisualStyleBackColor = true;
+            this.bntCAFile.Click += new System.EventHandler(this.btnCA_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(96, 153);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(75, 23);
+            this.btnCompare.TabIndex = 2;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.bntCAFile);
+            this.Controls.Add(this.btnRNfile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRNfile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button bntCAFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button btnCompare;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace ExcelCompare
+{
+    public partial class Form1 : Form
+    {
+        int RNFSheet = 2;
+        int RNFileColumn = 4;
+        int RNPathColumn = 7;
+        int RNVersionColumn = 6;
+
+        int RCASheet = 1;
+        int CAFileColumn = 1;
+        int CAPathColumn = 1;
+        int CAVersionColumn = 1;
+
+        string RNFile = string.Empty;
+        string CAFile = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRN_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+            }
+            Console.WriteLine(result); // <-- For d
+        }
+        
+        private void btnCA_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog2.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+            }
+            Console.WriteLine(result); // <-- For d
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            RNFile = openFileDialog1.FileName;
+        }
+      
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            CAFile = openFileDialog2.FileName;
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            CompareFiles();
+        }
+
+        public void CompareFiles()
+        {
+            List<FileItem> RNItems = new List<FileItem>();
+
+            Microsoft.Office.Interop.Excel.Application _excelApp = new Microsoft.Office.Interop.Excel.Application();
+            _excelApp.Visible = false;           
+
+            //open the workbook
+            Workbook workbook = _excelApp.Workbooks.Open(RNFile,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing);
+
+            //select the first sheet        
+            Worksheet worksheet = (Worksheet)workbook.Worksheets[RNFSheet];
+
+            //find the used range in worksheet
+            Range excelRange = worksheet.UsedRange;
+
+            //get an object array of all of the cells in the worksheet (their values)
+            object[,] valueArray = (object[,])excelRange.get_Value(
+                        XlRangeValueDataType.xlRangeValueDefault);
+
+            //access the cells
+           
+            for (int row = 1; row <= worksheet.UsedRange.Rows.Count; ++row)
+            {
+                if (valueArray[row, RNFileColumn] != null && !string.IsNullOrWhiteSpace(valueArray[row, RNFileColumn].ToString()))
+                {
+                    string fileName = valueArray[row, RNFileColumn].ToString();
+                    if (fileName.IndexOfAny(Path.GetInvalidFileNameChars()) < 0 && fileName.Contains("."))
+                    {
+                        FileItem RNItem = new FileItem();
+                        RNItem.FileName = fileName;
+                        if (valueArray[row, RNPathColumn] != null && !string.IsNullOrWhiteSpace(valueArray[row, RNPathColumn].ToString()))
+                        {
+                            RNItem.FilePath = valueArray[row, RNPathColumn].ToString();
+                        }
+                        if (valueArray[row, RNVersionColumn] != null && !string.IsNullOrWhiteSpace(valueArray[row, RNVersionColumn].ToString()))
+                        {
+                            RNItem.FileVersion = valueArray[row, RNVersionColumn].ToString();
+                        }
+                        int colorNumber = System.Convert.ToInt32(((Range)worksheet.Cells[row, RNFileColumn]).Interior.Color);
+                        Color color = System.Drawing.ColorTranslator.FromOle(colorNumber);
+                        RNItem.CellColor = color;
+                        RNItems.Add(RNItem);
+                    }
+                }
+               
+            }
+
+            //clean up stuffs
+            workbook.Close(false, Type.Missing, Type.Missing);
+            Marshal.ReleaseComObject(workbook);
+
+            _excelApp.Quit();
+            Marshal.FinalReleaseComObject(_excelApp);
+
+            //======================================================================
+
+            _excelApp = new Microsoft.Office.Interop.Excel.Application();
+            _excelApp.Visible = false;
+
+            //open the workbook
+            workbook = _excelApp.Workbooks.Open(RNFile,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing);
+
+            //select the first sheet        
+            worksheet = (Worksheet)workbook.Worksheets[2];
+
+            //find the used range in worksheet
+            excelRange = worksheet.UsedRange;
+
+            //get an object array of all of the cells in the worksheet (their values)
+            valueArray = (object[,])excelRange.get_Value(
+                        XlRangeValueDataType.xlRangeValueDefault);
+
+            //access the cells
+
+            for (int row = 1; row <= worksheet.UsedRange.Rows.Count; ++row)
+            {
+                if (valueArray[row, 4] != null && !string.IsNullOrWhiteSpace(valueArray[row, 4].ToString()))
+                {
+                    string fileName = valueArray[row, 4].ToString();
+                    if (fileName.IndexOfAny(Path.GetInvalidFileNameChars()) < 0 && fileName.Contains("."))
+                    {
+                        FileItem RNItem = new FileItem();
+                        RNItem.FileName = fileName;
+                        if (valueArray[row, 7] != null && !string.IsNullOrWhiteSpace(valueArray[row, 7].ToString()))
+                        {
+                            RNItem.FilePath = valueArray[row, 7].ToString();
+                        }
+                        if (valueArray[row, 6] != null && !string.IsNullOrWhiteSpace(valueArray[row, 6].ToString()))
+                        {
+                            RNItem.FileVersion = valueArray[row, 6].ToString();
+                        }
+                        int colorNumber = System.Convert.ToInt32(((Range)worksheet.Cells[row, 4]).Interior.Color);
+                        Color color = System.Drawing.ColorTranslator.FromOle(colorNumber);
+                        RNItem.CellColor = color;
+                        RNItems.Add(RNItem);
+                    }
+                }
+
+            }
+
+            //clean up stuffs
+            workbook.Close(false, Type.Missing, Type.Missing);
+            Marshal.ReleaseComObject(workbook);
+
+            _excelApp.Quit();
+            Marshal.FinalReleaseComObject(_excelApp);
+        }
+    }
+
+    public class FileItem
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string FileVersion { get; set; }
+        public Color CellColor { get; set; }
+    }
+}
