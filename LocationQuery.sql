7440cded9fe3f423f68faf8a3e4af9ca0aa95215
Vijay Karla <vijays80@gmail.com>
2019-02-13 11:58:16 +0530

    Update LocationQuery.sql

--- LocationQuery.sql
+++ LocationQuery.sql
@@ -6,3 +6,5 @@ SELECT TOP 500 LocationId, DATEADD(mi, 330, EVENTTIME) EventTime, Latitude, Long
 	END AS EventType
 FROM Location WHERE TrackId = 44
 ORDER BY LocationId DESC
+
+http://valetron.in/api/v2/mysql/_table/log?api_key=7d1a4baac442ed875b1545af24f9f4312a1cfdb3fad0db096db91cf2869d17f2&fields=lat,lon,logid,devid,time,speed,server_time,pint&order=logid%20DESC&limit=100&filter=(devid=861311006352792)
